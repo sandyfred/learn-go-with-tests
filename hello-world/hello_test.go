d578d0b307136492b7a278f7f6a7448f7684aa25
Sandy Fred <sandysam1525@gmail.com>
2024-06-22 19:14:05 +0530

    Integers

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Sandy")
+		got := Hello("Sandy", "")
 		want := "Hello, Sandy"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying 'Hello, World', when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Sandy", "Spanish")
+		want := "Hola, Sandy"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Sandy", "French")
+		want := "Bonjour, Sandy"
 		assertCorrectMessage(t, got, want)
 	})
 }
