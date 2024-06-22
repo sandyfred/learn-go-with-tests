c388b71b52d0f56ec1561fff0db13ce7e3983b0b
Sandy Fred <sandysam1525@gmail.com>
2024-06-22 19:47:28 +0530

    for-loop and benchmark

---
+++ iteration/repeat_test.go
@@ -0,0 +1,26 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a", 5)
+	want := "aaaaa"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("a", 5))
+	// Output: aaaaa
+}
